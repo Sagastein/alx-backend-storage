30b1dbae9fd4267679eaff99d5c204384916d6d9
Sagastein <ishimwe.sage.3@gmail.com>
2023-10-11 18:31:38 +0000

    uniqye users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,5 @@
+-- script that creates a table users with follwing fields
+-- id, email, name
 CREATE TABLE IF NOT EXISTS users (
 	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email varchar(255) NOT NULL UNIQUE,

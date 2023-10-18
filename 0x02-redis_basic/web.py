d0c3d81c8cb3e4cffbfa4500df848fb0ac40cab2
Sagastein <ishimwe.sage.3@gmail.com>
2023-10-18 10:20:41 -0700

    web py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,21 +1,38 @@
+#!/usr/bin/env python3
+'''A module with tools for request caching and tracking.
+'''
+import redis
 import requests
-from functools import lru_cache
-import time
+from functools import wraps
+from typing import Callable
 
-@lru_cache(maxsize=None)
+
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
+
+
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
 def get_page(url: str) -> str:
-    d
-    count_key = f"count:{url}"
-    count = cache.get(count_key, 0)
-    cache.set(count_key, count + 1, ex=10)
-
-    
-    page_key = f"page:{url}"
-    page = cache.get(page_key)
-    if page is None:
-        time.sleep(2)  # Simulate a slow response
-        page = requests.get(url).text
-        cache.set(page_key, page, ex=10)
-
-    return page
-
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
